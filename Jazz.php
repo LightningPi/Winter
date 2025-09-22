cc4dd2e7950c89a9d39fcb625c3b0fd027ecbd3a
LightningPi <91781766+LightningPi@users.noreply.github.com>
2025-09-22 15:57:02 +0800

    留言板主題：爵士樂

--- Jazz.php
+++ Jazz.php
@@ -1,7 +1,7 @@
 ﻿<?php session_start();?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
+<head> 
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Jazz</title>
@@ -87,3 +87,4 @@ echo "</center>";
 ?>
 </body>
 </html>
+
